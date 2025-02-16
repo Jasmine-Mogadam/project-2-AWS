59559ae5f59ff23633376822e16db7740e911ff8
Jasmine Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-02-15 22:40:28 -0500

    init

---
+++ flaskapp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello from Flask!'
+
+if __name__ == '__main__':
+  app.run()
+
+@app.route('/countme/<input_str>')
+def count_me(input_str):
+    input_counter = Counter(input_str)
+    response = []
+    for letter, count in input_counter.most_common():
+        response.append('"{}": {}'.format(letter, count))
+    return '<br>'.join(response)
